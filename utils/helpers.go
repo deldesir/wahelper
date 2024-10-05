3a1ac64d8d62f7bf7c5bb08cba38379f7ae43df0
Blondel MONDESIR <blondel.md@gmail.com>
2024-10-05 03:39:16 -0400

    Add JID parsing

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,1 +1,24 @@
+package utils
 
+import (
+    "go.mau.fi/whatsmeow/types"
+    "strings"
+)
+
+// ParseJID parses a string into a WhatsApp JID.
+func ParseJID(arg string) (types.JID, bool) {
+    if arg[0] == '+' {
+        arg = arg[1:]
+    }
+    if !strings.ContainsRune(arg, '@') {
+        return types.NewJID(arg, types.DefaultUserServer), true
+    }
+    jid, err := types.ParseJID(arg)
+    if err != nil {
+        return jid, false
+    }
+    if jid.User == "" {
+        return jid, false
+    }
+    return jid, true
+}
