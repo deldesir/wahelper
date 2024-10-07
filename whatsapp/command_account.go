117c3e697033a44ca6103828bb46a2208884aac6
Blondel MONDESIR <blondel.md@gmail.com>
2024-10-06 23:16:56 -0400

    Update command_account.go

--- whatsapp/command_account.go
+++ whatsapp/command_account.go
@@ -1,155 +1,213 @@
 package whatsapp
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"go.mau.fi/whatsmeow/appstate"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	"wahelper/utils"
+	"strconv"
+)
+
 func (c *Client) handlePairPhoneCommand(args []string) error {
-    if len(args) < 1 {
-        c.logger.Errorf("Usage: pair-phone <number>")
-        return nil
-    }
-    // ... implementation ...
-    return nil
+	if c.WAClient.IsLoggedIn() {
+		c.Logger.Infof("Already logged in")
+		return nil
+	}
+
+	qrChan, _ := c.WAClient.GetQRChannel(context.Background())
+	err := c.WAClient.Connect()
+	if err != nil {
+		c.Logger.Errorf("Failed to connect: %v", err)
+		return err
+	}
+
+	c.Logger.Infof("Please scan the QR code to login")
+	for evt := range qrChan {
+		if evt.Event == "code" {
+			// Print QR code to terminal
+			fmt.Printf("QR Code: %s\n", evt.Code)
+			// You can use a library like "github.com/mdp/qrterminal/v3" to display the QR code in the terminal
+			// qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+		} else {
+			c.Logger.Infof("Login event: %s", evt.Event)
+		}
+	}
+
+	return nil
 }
 
 func (c *Client) handleLogoutCommand(args []string) error {
-    err := c.cli.Logout()
-		if err != nil {
-			c.Logger.Errorf("Error logging out: %v", err)
-		} else {
-			c.Logger.Infof("Successfully logged out")
-		}
+	err := c.WAClient.Logout()
+	if err != nil {
+		c.Logger.Errorf("Error logging out: %v", err)
+	} else {
+		c.Logger.Infof("Successfully logged out")
+	}
+	return err
 }
 
 func (c *Client) handleSetPushNameCommand(args []string) error {
-    if len(args) == 0 {
-        log.Errorf("Usage: setpushname <name>")
-        return nil
-    }
-    err := c.cli.SendAppState(appstate.BuildSettingPushName(strings.Join(args, " ")))
-    if err != nil {
-        c.Logger.Errorf("Error setting push name: %v", err)
-    } else {
-        c.Logger.Infof("Push name updated")
-    }
+	if len(args) == 0 {
+		c.Logger.Errorf("Usage: setpushname <name>")
+		return nil
+	}
+	pushName := strings.Join(args, " ")
+	err := c.WAClient.SendAppState(appstate.BuildSettingPushName(pushName))
+	if err != nil {
+		c.Logger.Errorf("Error setting push name: %v", err)
+	} else {
+		c.Logger.Infof("Push name updated")
+	}
+	return err
 }
 
 func (c *Client) handleSetStatusCommand(args []string) error {
-    if len(args) == 0 {
-        log.Errorf("Usage: setstatus <message>")
-        return
-    }
-    err := cli.SetStatusMessage(strings.Join(args, " "))
-    if err != nil {
-        log.Errorf("Error setting status message: %v", err)
-    } else {
-        log.Infof("Status updated")
-    }
+	if len(args) == 0 {
+		c.Logger.Errorf("Usage: setstatus <message>")
+		return nil
+	}
+	statusMessage := strings.Join(args, " ")
+	err := c.WAClient.SetStatusMessage(statusMessage)
+	if err != nil {
+		c.Logger.Errorf("Error setting status message: %v", err)
+	} else {
+		c.Logger.Infof("Status updated")
+	}
+	return err
 }
 
 func (c *Client) handlePrivacySettingsCommand(args []string) error {
-    resp, err := cli.TryFetchPrivacySettings(false)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Printf("%+v\n", resp)
-		}
+	resp, err := c.WAClient.TryFetchPrivacySettings(false)
+	if err != nil {
+		c.Logger.Errorf("Error fetching privacy settings: %v", err)
+	} else {
+		c.Logger.Infof("Privacy settings: %+v", resp)
+	}
+	return err
 }
 
 func (c *Client) handleSetPrivacySettingCommand(args []string) error {
-    if len(args) < 2 {
-        log.Errorf("Usage: setprivacysetting <setting> <value>")
-        return
-    }
-    setting := types.PrivacySettingType(args[0])
-    value := types.PrivacySetting(args[1])
-    resp, err := cli.SetPrivacySetting(setting, value)
-    if err != nil {
-        fmt.Println(err)
-    } else {
-        fmt.Printf("%+v\n", resp)
-    }
+	if len(args) < 2 {
+		c.Logger.Errorf("Usage: setprivacysetting <setting> <value>")
+		return nil
+	}
+	setting := types.PrivacySettingType(args[0])
+	value := types.PrivacySetting(args[1])
+	resp, err := c.WAClient.SetPrivacySetting(setting, value)
+	if err != nil {
+		c.Logger.Errorf("Error setting privacy setting: %v", err)
+	} else {
+		c.Logger.Infof("Privacy setting updated: %+v", resp)
+	}
+	return err
 }
 
 func (c *Client) handleGetStatusPrivacyCommand(args []string) error {
-    resp, err := cli.GetStatusPrivacy()
-		fmt.Println(err)
-		fmt.Println(resp)
+	resp, err := c.WAClient.GetStatusPrivacy()
+	if err != nil {
+		c.Logger.Errorf("Error getting status privacy: %v", err)
+	} else {
+		c.Logger.Infof("Status privacy: %+v", resp)
+	}
+	return err
 }
 
 func (c *Client) handleSetDisappearTimerCommand(args []string) error {
-    if len(args) < 2 {
-        log.Errorf("Usage: setdisappeartimer <jid> <days>")
-        return
-    }
-    days, err := strconv.Atoi(args[1])
-    if err != nil {
-        log.Errorf("Invalid duration: %v", err)
-        return
-    }
-    recipient, ok := parseJID(args[0])
-    if !ok {
-        return
-    }
-    err = cli.SetDisappearingTimer(recipient, time.Duration(days)*24*time.Hour)
-    if err != nil {
-        log.Errorf("Failed to set disappearing timer: %v", err)
-    }
+	if len(args) < 2 {
+		c.Logger.Errorf("Usage: setdisappeartimer <jid> <days>")
+		return nil
+	}
+	days, err := strconv.Atoi(args[1])
+	if err != nil {
+		c.Logger.Errorf("Invalid duration: %v", err)
+		return nil
+	}
+	recipient, ok := utils.ParseJID(args[0])
+	if !ok {
+		c.Logger.Errorf("Invalid JID: %s", args[0])
+		return nil
+	}
+	duration := time.Duration(days) * 24 * time.Hour
+	err = c.WAClient.SetDisappearingTimer(recipient, duration)
+	if err != nil {
+		c.Logger.Errorf("Failed to set disappearing timer: %v", err)
+	} else {
+		c.Logger.Infof("Disappearing timer set for %s to %d days", recipient.String(), days)
+	}
+	return err
 }
 
 func (c *Client) handleSetDefaultDisappearTimerCommand(args []string) error {
-    if len(args) < 1 {
-        log.Errorf("Usage: setdefaultdisappeartimer <days>")
-        return
-    }
-    days, err := strconv.Atoi(args[0])
-    if err != nil {
-        log.Errorf("Invalid duration: %v", err)
-        return
-    }
-    err = cli.SetDefaultDisappearingTimer(time.Duration(days) * 24 * time.Hour)
-    if err != nil {
-        log.Errorf("Failed to set default disappearing timer: %v", err)
-    }
+	if len(args) < 1 {
+		c.Logger.Errorf("Usage: setdefaultdisappeartimer <days>")
+		return nil
+	}
+	days, err := strconv.Atoi(args[0])
+	if err != nil {
+		c.Logger.Errorf("Invalid duration: %v", err)
+		return nil
+	}
+	duration := time.Duration(days) * 24 * time.Hour
+	err = c.WAClient.SetDefaultDisappearingTimer(duration)
+	if err != nil {
+		c.Logger.Errorf("Failed to set default disappearing timer: %v", err)
+	} else {
+		c.Logger.Infof("Default disappearing timer set to %d days", days)
+	}
+	return err
 }
 
 func (c *Client) handleGetBlockListCommand(args []string) error {
-    blocklist, err := cli.GetBlocklist()
-		if err != nil {
-			log.Errorf("Failed to get blocked contacts list: %v", err)
-		} else {
-			log.Infof("Blocklist: %+v", blocklist)
-		}
+	blocklist, err := c.WAClient.GetBlocklist()
+	if err != nil {
+		c.Logger.Errorf("Failed to get blocked contacts list: %v", err)
+	} else {
+		c.Logger.Infof("Blocklist: %+v", blocklist)
+	}
+	return err
 }
 
 func (c *Client) handleBlockCommand(args []string) error {
-    if len(args) < 1 {
-        log.Errorf("Usage: block <jid>")
-        return
-    }
-    jid, ok := parseJID(args[0])
-    if !ok {
-        return
-    }
-    resp, err := cli.UpdateBlocklist(jid, events.BlocklistChangeActionBlock)
-    if err != nil {
-        log.Errorf("Error updating blocklist: %v", err)
-    } else {
-        log.Infof("Blocklist updated: %+v", resp)
-    }
+	if len(args) < 1 {
+		c.Logger.Errorf("Usage: block <jid>")
+		return nil
+	}
+	jid, ok := utils.ParseJID(args[0])
+	if !ok {
+		c.Logger.Errorf("Invalid JID: %s", args[0])
+		return nil
+	}
+	resp, err := c.WAClient.UpdateBlocklist(jid, events.BlocklistChangeActionBlock)
+	if err != nil {
+		c.Logger.Errorf("Error updating blocklist: %v", err)
+	} else {
+		c.Logger.Infof("Blocked %s: %+v", jid.String(), resp)
+	}
+	return err
 }
 
 func (c *Client) handleUnblockCommand(args []string) error {
-    if len(args) < 1 {
-        log.Errorf("Usage: unblock <jid>")
-        return
-    }
-    jid, ok := parseJID(args[0])
-    if !ok {
-        return
-    }
-    resp, err := cli.UpdateBlocklist(jid, events.BlocklistChangeActionUnblock)
-    if err != nil {
-        log.Errorf("Error updating blocklist: %v", err)
-    } else {
-        log.Infof("Blocklist updated: %+v", resp)
-    }
+	if len(args) < 1 {
+		c.Logger.Errorf("Usage: unblock <jid>")
+		return nil
+	}
+	jid, ok := utils.ParseJID(args[0])
+	if !ok {
+		c.Logger.Errorf("Invalid JID: %s", args[0])
+		return nil
+	}
+	resp, err := c.WAClient.UpdateBlocklist(jid, events.BlocklistChangeActionUnblock)
+	if err != nil {
+		c.Logger.Errorf("Error updating blocklist: %v", err)
+	} else {
+		c.Logger.Infof("Unblocked %s: %+v", jid.String(), resp)
+	}
+	return err
 }
-
