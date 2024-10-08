dce823592744bada6319531768f10c39d35b7a57
Blondel MONDESIR <blondel.md@gmail.com>
2024-10-07 23:14:44 -0400

    Update command_media.go

--- whatsapp/command_media.go
+++ whatsapp/command_media.go
@@ -1,46 +1,66 @@
 package whatsapp
 
+import (
+	"strings"
+
+	"go.mau.fi/whatsmeow"
+	"wahelper/utils"
+)
+
 func (c *Client) handleMediaConnCommand(args []string) error {
-    conn, err := c.cli.DangerousInternals().RefreshMediaConn(false)
-    if err != nil {
-        c.logger.Errorf("Failed to get media connection: %v", err)
-    } else {
-        c.logger.Infof("Media connection: %+v", conn)
-    }
-    return nil
+	conn, err := c.WAClient.DangerousInternals().RefreshMediaConn(false)
+	if err != nil {
+		c.Logger.Errorf("Failed to get media connection: %v", err)
+	} else {
+		c.Logger.Infof("Media connection: %+v", conn)
+	}
+	return err
 }
 
 func (c *Client) handleGetAvatarCommand(args []string) error {
-    if len(args) < 1 {
-        c.Logger.Errorf("Usage: getavatar <jid> [existing ID] [--preview] [--community]")
-        return
-    }
-    jid, ok := parseJID(args[0])
-    if !ok {
-        return
-    }
-    existingID := ""
-    if len(args) > 2 {
-        existingID = args[2]
-    }
-    var preview, isCommunity bool
-    for _, arg := range args {
-        if arg == "--preview" {
-            preview = true
-        } else if arg == "--community" {
-            isCommunity = true
-        }
-    }
-    pic, err := c.cli.GetProfilePictureInfo(jid, &whatsmeow.GetProfilePictureParams{
-        Preview:     preview,
-        IsCommunity: isCommunity,
-        ExistingID:  existingID,
-    })
-    if err != nil {
-        c.Logger.Errorf("Failed to get avatar: %v", err)
-    } else if pic != nil {
-        c.Logger.Infof("Got avatar ID %s: %s", pic.ID, pic.URL)
-    } else {
-        c.Logger.Infof("No avatar found")
-    }
+	if len(args) < 1 {
+		c.Logger.Errorf("Usage: getavatar <jid> [existing ID] [--preview] [--community]")
+		return nil
+	}
+	jid, ok := utils.ParseJID(args[0])
+	if !ok {
+		c.Logger.Errorf("Invalid JID: %s", args[0])
+		return nil
+	}
+	existingID := ""
+	var preview, isCommunity bool
+	for _, arg := range args[1:] {
+		if strings.HasPrefix(arg, "--") {
+			switch arg {
+			case "--preview":
+				preview = true
+			case "--community":
+				isCommunity = true
+			default:
+				c.Logger.Errorf("Unknown flag: %s", arg)
+				return nil
+			}
+		} else {
+			if existingID == "" {
+				existingID = arg
+			} else {
+				c.Logger.Errorf("Unexpected argument: %s", arg)
+				return nil
+			}
+		}
+	}
+	pic, err := c.WAClient.GetProfilePictureInfo(jid, &whatsmeow.GetProfilePictureParams{
+		Preview:     preview,
+		IsCommunity: isCommunity,
+		ExistingID:  existingID,
+	})
+	if err != nil {
+		c.Logger.Errorf("Failed to get avatar: %v", err)
+		return err
+	} else if pic != nil {
+		c.Logger.Infof("Got avatar ID %s: %s", pic.ID, pic.URL)
+	} else {
+		c.Logger.Infof("No avatar found")
+	}
+	return nil
 }
